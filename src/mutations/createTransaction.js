a15a2b53e1a55e96670f01bc4a2fb611522ee08b
shoaibfarooq3 <shoaibfarooq3@gmail.com>
2023-05-26 00:37:37 -0700

    Version Updation

--- src/mutations/createTransaction.js
+++ src/mutations/createTransaction.js
@@ -1,11 +1,9 @@
-
 export default async function createTransaction(context, input) {
-    const { Transactions } = context.collections;
-    const transactionsResult = await Transactions.insertOne(input);
-    if (transactionsResult.ops) {
-        return transactionsResult.ops[0];
-    }
-    else{
-        return []
-    }
+  const { Transactions } = context.collections;
+  const transactionsResult = await Transactions.insertOne(input);
+  if (transactionsResult.ops) {
+    return transactionsResult.ops[0];
+  } else {
+    return [];
+  }
 }
--- src/resolvers/Mutation/createTransaction.js
+++ src/resolvers/Mutation/createTransaction.js
@@ -1,9 +1,12 @@
 import convertKeysToLowercase from "../../util/convertKeysToLowercase.js";
 
 export default async function createTransaction(_, { input }, context) {
-    const convertedInput = await convertKeysToLowercase(input);
-    const date = new Date();
-    convertedInput.createdAt = date;
-    const transactionResponse= await context.mutations.createTransaction(context, convertedInput);
-    return transactionResponse;
+  const convertedInput = await convertKeysToLowercase(input);
+  const date = new Date();
+  convertedInput.createdAt = date;
+  const transactionResponse = await context.mutations.createTransaction(
+    context,
+    convertedInput
+  );
+  return transactionResponse;
 }
